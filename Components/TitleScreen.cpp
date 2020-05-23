f6d848c637b222c09843c432098ef59bbb22d9e8
Robbie Hammond <58093703+robbiehammond@users.noreply.github.com>
2020-05-22 22:48:58 -0400

    Add files via upload

--- Components/TitleScreen.cpp
+++ Components/TitleScreen.cpp
@@ -1,10 +1,11 @@
 #include "TitleScreen.h"
+#include <iostream>
 #pragma once
 
 
 TitleScreen::TitleScreen(sf::RenderWindow& _window, sf::Font& _font)
 	: projectDesc("A fun quarantine project by Robbie Hammond\n"
-		"https://github.com/robbiehammond/Simulation", _font), window(_window), font(_font)
+		"https://github.com/robbiehammond/Simulation", _font), window(_window), font(_font), mapChosen(Map0)
 {
 	projectDesc.setCharacterSize(10);
 	projectDesc.setPosition(1060, 690);
@@ -15,10 +16,6 @@ TitleScreen::TitleScreen(sf::RenderWindow& _window, sf::Font& _font)
 	title.setFillColor(sf::Color::White);
 }
 
-sf::Text TitleScreen::buildTitle()
-{
-	return sf::Text();
-}
 
 sf::RectangleShape TitleScreen::createMapBox(float x, float y)
 {
@@ -53,15 +50,75 @@ sf::Sprite TitleScreen::createMapImages(sf::Texture& tex, float x, float y)
 	return mapSprite;
 }
 
-sf::RectangleShape TitleScreen::createOtherBox(float x, float y, float width, float height)
+sf::RectangleShape TitleScreen::createOtherBox(float x, float y, float width, float height, sf::Color color)
 {
 	sf::RectangleShape box(sf::Vector2f(width, height));
 	box.setPosition(x, y);
-	box.setOutlineColor(sf::Color::Red);
+	box.setOutlineColor(color);
 	box.setOutlineThickness(2);
 	box.setFillColor(sf::Color::Black);
-	return sf::RectangleShape();
+	return box;
 }
+
+sf::Text TitleScreen::createOtherText(float x, float y, float size, sf::Color color, string tex)
+{
+	sf::Text words(tex, font);
+	words.setPosition(sf::Vector2f(x, y));
+	words.setFillColor(color);
+	words.setCharacterSize(size);
+	return words;
+}
+
+
+bool TitleScreen::clickInRange(sf::RectangleShape& object)
+{
+	sf::Vector2f mouseCoords = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+	sf::FloatRect bound = object.getGlobalBounds();
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && bound.contains(mouseCoords))
+		return true;
+	else 
+		return false;
+}
+
+void TitleScreen::setColorOnClick(sf::RectangleShape& box0, sf::RectangleShape& box1, sf::RectangleShape& box2, sf::RectangleShape& box3)
+{
+	if (clickInRange(box0)) {
+		mapChosen = Map0;
+		box0.setOutlineColor(sf::Color::Blue);
+		box1.setOutlineColor(sf::Color::White);
+		box2.setOutlineColor(sf::Color::White);
+		box3.setOutlineColor(sf::Color::White);
+	}
+	if (clickInRange(box1)) {
+		mapChosen = Map1;
+		box0.setOutlineColor(sf::Color::White);
+		box1.setOutlineColor(sf::Color::Blue);
+		box2.setOutlineColor(sf::Color::White);
+		box3.setOutlineColor(sf::Color::White);
+	}
+	if (clickInRange(box2)) {
+		mapChosen = Map2;
+		box0.setOutlineColor(sf::Color::White);
+		box1.setOutlineColor(sf::Color::White);
+		box2.setOutlineColor(sf::Color::Blue);
+		box3.setOutlineColor(sf::Color::White);
+	}
+	if (clickInRange(box3)) {
+		mapChosen = Map3;
+		box0.setOutlineColor(sf::Color::White);
+		box1.setOutlineColor(sf::Color::White);
+		box2.setOutlineColor(sf::Color::White);
+		box3.setOutlineColor(sf::Color::Blue);
+	}
+}
+
+Map TitleScreen::getChosenMap()
+{
+	return mapChosen;
+}
+
+
+
 
 
 
@@ -79,22 +136,31 @@ void TitleScreen::loadScreen()
 	sf::Text map2text = createBoxText(" Map 2", 400, 400);
 	sf::Texture map2texture = getMapImages("Map2.png");
 	sf::Sprite map2sprite = createMapImages(map2texture, 400, 305);
-
 	
 	sf::RectangleShape map3box = createMapBox(780, 300);
 	sf::Text map3text = createBoxText(" Map 3", 780, 400);
 	//sf::Texture map3texture = getMapImages("Map3.png");
 	//sf::Sprite map3sprite = createMapImages(map3texture, 780, 100);
-	
+
+
+	sf::RectangleShape conflictBox = createOtherBox(910, 500, 270, 50, sf::Color::Red);
+	sf::Text conflictText = createOtherText(910, 500, 30, sf::Color::Red, " Play Conflict Mode");
+	sf::RectangleShape sandboxBox = createOtherBox(100, 500, 270, 50, sf::Color::Green);
+	sf::Text sandboxText = createOtherText(100, 500, 30, sf::Color::Green, "Play Sandbox Mode");
+	sf::RectangleShape aboutBox = createOtherBox(590, 500, 100, 50, sf::Color::Blue);
+	sf::Text aboutText = createOtherText(600, 500, 30, sf::Color::Blue, "About");
 
 	bool playingGame = false;
 	while (window.isOpen() && !playingGame) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
-			if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+			if (clickInRange(conflictBox))
 			{
 				playingGame = true;
 			}
+
+		setColorOnClick(map0box, map1box, map2box, map3box);
+
 		}
 		window.clear();
 		window.draw(map0box);
@@ -114,6 +180,13 @@ void TitleScreen::loadScreen()
 
 		window.draw(projectDesc);
 		window.draw(title);
+
+		window.draw(conflictBox);
+		window.draw(conflictText);
+		window.draw(sandboxBox);
+		window.draw(sandboxText);
+		window.draw(aboutBox);
+		window.draw(aboutText);
 
 		window.display();
 	}
